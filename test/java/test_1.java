3a259545e387e4949ebf09b133256ee63ae60340
Jack Joe <lancereally@foxmail.com>
2020-11-30 16:30:56 +0800

    update: git演示模块，test_1文件

--- test/java/test_1.java
+++ test/java/test_1.java
@@ -1,8 +1,6 @@
 public class test_1 {
     public static void main(String[] args) {
-        class JDI{
-            String str;
-        }
-        System.out.println("酸根东仍终端?");
+        int t = 233;
+        System.out.println("国核信息");
     }
 }
