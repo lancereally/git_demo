41d1db6973e76b5f0e5affd00d253aeec51078c3
zsb514 <459401748@qq.com>
2020-12-02 17:30:57 +0800

    qwerw

--- test/java/test_1.java
+++ test/java/test_1.java
@@ -1,7 +1,9 @@
 public class test_1 {
     public static void main(String[] args) {
+        int a = 123;
         class Jiuzhe{
             char a = 's';
+            System.out.println("国核信息");
         }
     }
 }
