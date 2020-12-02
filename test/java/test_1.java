1440c7d6362ec3188878cf956c6a991ba2ec6b8d
zsb514 <459401748@qq.com>
2020-12-02 17:21:57 +0800

    update: git_demo, test_1

--- test/java/test_1.java
+++ test/java/test_1.java
@@ -1,7 +1,7 @@
 public class test_1 {
     public static void main(String[] args) {
-        int t = 233;
-        String s = "国核信息";
-
+        class Jiuzhe{
+            char a = 's';
+        }
     }
 }
