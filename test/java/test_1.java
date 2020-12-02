909e81d18aa661bd75157807cbddb6853f6ef79d
zsb514 <459401748@qq.com>
2020-12-02 17:34:57 +0800

    affasd

--- test/java/test_1.java
+++ test/java/test_1.java
@@ -3,7 +3,8 @@ public class test_1 {
         int a = 123;
         class Jiuzhe{
             char a = 's';
-            System.out.println("国核信息");
         }
+        System.out.println("国核信息");
+    
     }
 }
