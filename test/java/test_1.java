34e82c7877bf5ef5ff47e1aaacb5c595565987e4
Jack Joe <lancereally@foxmail.com>
2020-12-07 11:44:56 +0800

    update: git演示模块，test_1文件.ver5

--- test/java/test_1.java
+++ test/java/test_1.java
@@ -1,9 +1,9 @@
 public class test_1 {
     public static void main(String[] args) {
         class Jiuzhe{
-            String b = "s";
+            String b = "ssss";
         }
         System.out.println("国核信息");
-        //a---z//
+        //a-z//
     }
 }
