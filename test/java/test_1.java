337b76e2934a966c539849d5e4f3eddbeba61722
张尚斌 <zhangshangbin01@spnic.com.cn>
2020-12-07 11:28:10 +0800

    nqbi

--- test/java/test_1.java
+++ test/java/test_1.java
@@ -1,7 +1,7 @@
 public class test_1 {
     public static void main(String[] args) {
         class Jiuzhe{
-            String a = 's';
+            int a = 's';
         }
         System.out.println("国核信息");
         //haoqilail
