bdf1ddc538e70d2604f6ae4a7be74f77352ce0d8
张尚斌 <zhangshangbin01@spnic.com.cn>
2020-12-07 11:10:36 +0800

    update: git_demo,第三版

--- test/java/test_1.java
+++ test/java/test_1.java
@@ -1,11 +1,11 @@
 public class test_1 {
     public static void main(String[] args) {
         class Jiuzhe{
-            char a = 's';
+            String a = 's';
         }
         System.out.println("国核信息");
         //haoqilail
-        //az//
+        //a-z//
         //
     }
 }
