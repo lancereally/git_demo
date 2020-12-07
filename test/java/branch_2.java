4f6b7b101ff4666fadac69cc3a3315171326085b
Jack Joe <lancereally@foxmail.com>
2020-12-07 14:01:03 +0800

    update: git

--- test/java/branch_2.java
+++ test/java/branch_2.java
@@ -1,3 +1,4 @@
 public class branch_2 {
     String s = " ";
+    //国核
 }
--- test/java/test_1.java
+++ test/java/test_1.java
@@ -1,11 +1,12 @@
 public class test_1 {
     public static void main(String[] args) {
+        String s = "123";
         class Jiuzhe{
-            String a ="";
+            char a = 's';
         }
         System.out.println("国核信息");
         //haoqilail
-        //a-z//
-        //
+        //az//
+//
     }
 }
